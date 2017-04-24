5b5ecad76aa65871f01faa46e8e8d30070c1933e
Simon Hourlier <simon.hourlier@phelma.grenoble-inp.fr>
2017-04-24 10:55:36 +0200

    routes

--- routes/ctrl.js
+++ routes/ctrl.js
@@ -1,6 +1,14 @@
-Unstaged changes after reset:
-M	init.sql
-M	main.js
-M	routes/ctrl.js
-M	services/projet.js
-D	www/ctrl.js
+var express = require('express');
+var router = express.Router();
+var service = require('../services/projet')
+
+router.get('/user/contacts/:id', service.listContacts);
+router.get('/user/pos/:id', service.getPosition);
+router.get('/user/pos/:id', service.updatePosition);
+router.post('/user/media/:type/:id', service.getMedia);
+router.put('/user/', service.updateUser);
+router.get('/user', service.addUser);
+router.get('/user', service.deleteUser);
+        
+module.exports = router
+
