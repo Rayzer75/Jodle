a5437abb359cee9785a88aaa92e1aaf337ecbf13
Simon Hourlier <simon.hourlier@phelma.grenoble-inp.fr>
2017-04-24 11:26:08 +0200

    legere modif

--- services/projet.js
+++ services/projet.js
@@ -1,1 +1,33 @@
+var db = require('../db/pq_projet.js')
+
+function listContacts(req, res) { 
+    
+    db.getContact(telephone,function(error,data)
+     {
+         if (error == null)
+         {
+             res.status(200).render('contacts', {fonctions : data});
+             console.log(data);
+         }
+         else
+         {
+             console.log(error);
+             res.status(500).send(error);
+         }
+    })
+}
+
+function addUser(req,res){
+        var pseudo = req.body.pseudo
+        var mdp = req.body.mdp
+        var nom = req.body.nom
+        var prenom = req.body.prenom
+        var tel = req.body.tel
+        
+        db.addUser(tel, pseudo, mdp, nom, prenom, 1,function () {
+        res.status(200).send("ok");
+    })
+    }
+        
+
 
